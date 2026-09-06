73aea96a0962c853cd6a3e5bd6ef3b5648e94cd3
cometduyent-hub <comet.duyent@gmail.com>
2026-09-06 18:16:20 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,21 @@
-import "./globals.css";
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Physics Test Arena",
-  description: "Hệ thống kiểm tra online Vật lí"
+  description: "Hệ thống kiểm tra trực tuyến môn Vật lí",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="vi">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.8/katex.min.css" />
+      </head>
       <body>{children}</body>
     </html>
   );
